53cd6a31bddbc3fb057fe0eca9532bb778ec6abb
Quentin Calvez <quentin.calvez@quentez.com>
2025-03-11 23:38:18 +0900

    Add more link detection tests.

--- packages/lib-sdk/src/model/records/__tests__/record.test.ts
+++ packages/lib-sdk/src/model/records/__tests__/record.test.ts
@@ -1,4 +1,5 @@
 import {describe, expect, test} from "vitest";
+import {AppRecord} from "../../recordTypes/appRecord.js";
 import {EntityRecord} from "../../recordTypes/entityRecord.js";
 import {SubscriptionRecord} from "../../recordTypes/subscriptionRecord.js";
 import {Record} from "../record.js";
@@ -60,6 +61,96 @@ describe("findLinks()", () => {
           "value": {
             "entity": "system.baq.dev",
             "recordId": "80be958368dd414fabb9420647daa1ec",
+          },
+        },
+      ]
+    `);
+  });
+
+  test("Find links in App record", () => {
+    // Prepare.
+    const date = new Date("2025-03-10T01:01:01.000Z");
+
+    const record: AppRecord = {
+      author: {entity: "author.host.com"},
+      id: "d9f0eb28a5594fd09b1ebaef7c8cf0f9",
+      source: "self",
+      createdAt: date,
+      receivedAt: undefined,
+      version: undefined,
+      type: AppRecord.type,
+      content: {
+        name: "My app",
+        icon: {
+          hash: "f51380bb4a2742ab8b5034ad471d3403d96264fcd3b744d89b9dc86cb6524bc7",
+          name: "icon.png",
+          type: "image/png",
+        },
+        uris: {
+          redirect: "https://app.com",
+        },
+        scopeRequest: {
+          read: [
+            {
+              entity: "types.baq.dev",
+              recordId: "38fdbec0b0424ff3bb2168217c2b5e3a",
+            },
+            {
+              entity: "types.baq.dev",
+              recordId: "fed16df89b6440bcb8230ff994f117c0",
+            },
+          ],
+        },
+      },
+      permissions: RecordPermissions.public,
+      mode: "synced",
+    };
+
+    // Act.
+    const links = Record.findLinks(AppRecord, record);
+
+    // Assert.
+    expect(links).toMatchInlineSnapshot(`
+      [
+        {
+          "path": "$['author']",
+          "type": "ENTITY",
+          "value": {
+            "entity": "author.host.com",
+          },
+        },
+        {
+          "path": "$['type']",
+          "type": "VERSION",
+          "value": {
+            "entity": "system.baq.dev",
+            "recordId": "58d114d7dcd24b1aa1ccbbccfe77634a",
+            "versionHash": "df617207dd07be4efd1837ae5b364c018990868085663ac905dc1ba68c75e247",
+          },
+        },
+        {
+          "path": "$['content']['scope_request']['read'][*]",
+          "type": "RECORD",
+          "value": {
+            "entity": "types.baq.dev",
+            "recordId": "38fdbec0b0424ff3bb2168217c2b5e3a",
+          },
+        },
+        {
+          "path": "$['content']['scope_request']['read'][*]",
+          "type": "RECORD",
+          "value": {
+            "entity": "types.baq.dev",
+            "recordId": "fed16df89b6440bcb8230ff994f117c0",
+          },
+        },
+        {
+          "path": "$['content']['icon']",
+          "type": "BLOB",
+          "value": {
+            "hash": "f51380bb4a2742ab8b5034ad471d3403d96264fcd3b744d89b9dc86cb6524bc7",
+            "name": "icon.png",
+            "type": "image/png",
           },
         },
       ]
